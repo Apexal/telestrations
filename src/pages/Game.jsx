66fc08eafcef45ab6883fef9fa1fbc3273cc1e61
Frank Matranga <thefrankmatranga@gmail.com>
2021-04-15 21:07:08 -0400

    Read max players from state

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -19,6 +19,7 @@ class Game extends Component {
       isInGame: false,
       hostPlayerClientId: '',
       errorMessage: null,
+      maxPlayers: 1,
       players: {}
     }
 
@@ -38,6 +39,8 @@ class Game extends Component {
       changes.forEach(change => {
         if (change.field === 'hostPlayerClientId') {
           this.setState({ hostPlayerClientId: change.value })
+        } else if (change.field === 'maxPlayers') {
+          this.setState({ maxPlayers: change.value })
         }
       })
     }
@@ -85,7 +88,7 @@ class Game extends Component {
   async componentDidMount () {
     const roomId = this.getGameRoomId()
 
-    this.setState({ roomId });
+    this.setState({ roomId })
 
     // There are two possibilities at this point
     // 1. The player clicked host game and has already created and joined a game room
@@ -125,21 +128,22 @@ class Game extends Component {
   }
 
   componentWillUnmount () {
-    this.state.room.removeAllListeners()
-    this.state.room.leave()
+    const room = getGameRoom()
+    room.removeAllListeners()
+    room.leave()
   }
 
-  renderPlayers() {
+  renderPlayers () {
     return Object.keys(this.state.players).map(key => (
-      <PlayerTag key={key} isHost={this.state.hostPlayerClientId === key} displayName={this.state.players[key].displayName}></PlayerTag>
-    ));
+      <PlayerTag key={key} isHost={this.state.hostPlayerClientId === key} displayName={this.state.players[key].displayName} />
+    ))
   }
 
   render () {
     const isHost = this.state.hostPlayerClientId === this.state.sessionId
 
     const playerCount = Object.keys(this.state.players).length
-    const playerListItems = this.renderPlayers();
+    const playerListItems = this.renderPlayers()
     return (
       <div>
         {this.state.isJoiningGame && (
@@ -150,8 +154,8 @@ class Game extends Component {
         {this.state.isInGame &&
           <div>
             <h1>{this.state.roomId}</h1>
-            <h5>{playerCount} / 12 players</h5>
-  
+            <h5>{playerCount} / {this.state.maxPlayers} players</h5>
+
             <ul>
               {playerListItems}
             </ul>
