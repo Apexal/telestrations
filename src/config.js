55c0ebf56557d024b3621ad79617051d3b7c5876
Frank Matranga <thefrankmatranga@gmail.com>
2021-05-12 22:11:54 -0400

    Point to Heroku server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   minPlayers: 1, // TODO: change in prod
-  serverURL: 'localhost:2567', // TODO: change in prod
+  serverURL: 'telestrations-server.herokuapp.com', // TODO: change in prod
   thickWidth: 10,
   thinWidth: 5,
   colors: ['black', 'red', 'green', 'blue', 'yellow']
